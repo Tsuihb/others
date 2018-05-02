9ba64d37902a7e867f97635c10c2834654042572
Tsuihb <m15112284553@163.com>
2018-05-03 02:14:14 +0800

    add index.js and package.json

--- wechat/index.js
+++ wechat/index.js
@@ -1,24 +1,33 @@
 var Koa = require('koa');
-var path = require('path');
-var wechat = require('./wechat/g');
-var util = require('./libs/util');
-var wechat_file = path.join(__dirname, './config/wechat.txt');
+var sha1 = require('sha-1');
 var config = {
-    wechat:{
-        appID:'wxedf713ba56b197b7', //填写你自己的appID
-        appSecret:'b637b0021c887486918a2aeb2724518d',  //填写你自己的appSecret
-        token:'testOne',  //填写你自己的token
-        getAccessToken: function() {
-            return util.readFileAsync(wechat_file);
-        },
-        saveAccessToken: function(data) {
-            data = JSON.stringify(data);
-            return util.writeFileAsync(wechat_file);
-        }
+    weChat:{
+        appID:'wxedf713ba56b197b7',
+        appSecret:'b637b0021c887486918a2aeb2724518d',
+        token:'testOne'
     }
-};
+}
 
 var app = new Koa();
-app.use(wechat(config.wechat));
+
+app.use(async function(ctx, next) {
+    console.log(ctx.query);
+    var token = config.weChat.token;
+    var signature = ctx.signature;
+    var nonce = ctx.query.nonce;
+    var timestamp = ctx.query.timestamp;
+    var echostr = ctx.query.echostr;
+    var str = [token, timestamp, nonce].sort().join('');
+    var sha = sha1(str);
+    
+    if (sha == signature) {
+        ctx.body = echostr
+    } else {
+        ctx.body = 'wrong';
+    }
+    console.log('本地加密消息：'+sha)
+    
+});
+
 app.listen(80);
-console.log('the port at 80')
+console.log('the sever start at port 80')
