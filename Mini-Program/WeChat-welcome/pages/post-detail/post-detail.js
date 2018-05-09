c0689e579586b6600f07f07570dbac4253531099
Tsuihb <m15112284553@163.com>
2018-05-09 21:51:45 +0800

    v4.0

--- Mini-Program/WeChat-welcome/pages/post-detail/post-detail.js
+++ Mini-Program/WeChat-welcome/pages/post-detail/post-detail.js
@@ -14,58 +14,54 @@ Page({
    */
   onLoad: function (options) {
     var postId = options.id;
-    /* this.data = detailData.post_list[postId];
-    console.log(this.data) */
+    this.data.currentTarget = postId;
     this.setData(detailData.post_list[postId]);
-    wx.setStorageSync("key", "one")
-  },
+    //收藏开始
+    //初始化代码
+    var postsCollected = wx.getStorageSync('posts_collected');
+    if (postsCollected) {
+      var postCollected = postsCollected[postId]
+      this.setData({
+        collected: postCollected
+      });
+    } else {
+      var postsCollected = {}
+      postsCollected[postId] = false;
+      wx.setStorageSync('posts_collected', postsCollected);
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  //点击时候的代码
+  onColletionTap: function (event) {
+    //拿到这个缓存的值
+    var postsCollected = wx.getStorageSync('posts_collected');
+    //拿到这个值
+    var postCollected = postsCollected[this.data.currentPostId];
+    //取反操作 收藏的变成未收藏
+    postCollected = !postCollected;
+    postsCollected[this.data.currentPostId] = postCollected;
+    //更新文章是否的缓存值
+    wx.setStorageSync('posts_collected', postsCollected);
+    //更新数据绑定变量，从而实现切换图片
+    this.setData({
+      collected: postCollected
+    });
+    wx.showToast({
+      title: postCollected ? '收藏成功' : '取消成功',
+      duration: 800,
+      icon: 'success'
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  musicOntap: function() {
+    const innerAudioContext = wx.createInnerAudioContext()
+    innerAudioContext.autoplay = true
+    innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+    innerAudioContext.onPlay(() => {
+      console.log('开始播放')
+    })
+    innerAudioContext.onError((res) => {
+      console.log(res.errMsg)
+      console.log(res.errCode)
+    })
   }
 })
